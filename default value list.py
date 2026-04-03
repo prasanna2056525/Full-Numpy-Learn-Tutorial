7605c921911e43d2a9d415df66d0b2ae0787909b
Prasanna Gautam <prasannagautam10@gmail.com>
2026-04-03 17:41:03 +0545

    Numpy Learning Basics

---
+++ default value list.py
@@ -0,0 +1,12 @@
+import numpy as np 
+
+zeroes_array = np.zeros((3,3))
+print(zeroes_array)
+
+
+ones_array = np.ones((2,3))
+print(ones_array)
+
+
+filles_array = np.full((2,2),7)
+print( filles_array)
---
+++ multidarray.py
@@ -0,0 +1,5 @@
+import numpy as np 
+
+matrix = np.array([[2,4,6],
+                   [7,8,9]])
+print(matrix)
